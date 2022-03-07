cc9def7b63a0297990b45bd9a2a03bd942ded83e
Luis José Salamó Esteve <lsalamo@gmail.com>
2022-03-07 18:33:24 +0100

    test

--- adobe/health-metrics-comparison/health-metrics-comparison.py
+++ adobe/health-metrics-comparison/health-metrics-comparison.py
@@ -117,6 +117,7 @@ def get_google_analytics_data():
     df.drop(df.index[:1], inplace=True)
     df.drop(['Totals', 'Totals.1'], axis=1, inplace=True)
     df.columns = ['day', 'web-visits', 'web-visitors', 'and-visits', 'and-visitors', 'ios-visits', 'ios-visitors']
+    # df.columns = ['day', 'and-visits', 'and-visitors', 'web-visits', 'web-visitors', 'ios-visits', 'ios-visitors']
     df['day'] = df['day'].astype('int64').astype('str')
 
     print('> get_google_analytics_data() -', 'clean dataframe loaded')
@@ -149,26 +150,29 @@ def get_data(rs):
     print('')
     df_aa = result['df_aa'].loc[result['df_aa'].rs == rs]
     df = df_aa.join(result['df_ga'].set_index('day'), on='day',lsuffix='-aa', rsuffix='-ga')
+    
     result['df_web'] = df[['day', 'web-visits-aa', 'web-visitors-aa', 'web-visits-ga', 'web-visitors-ga']]
     result['df_android'] = df[['day', 'and-visits-aa', 'and-visitors-aa', 'and-visits-ga', 'and-visitors-ga']]
     result['df_ios'] = df[['day', 'ios-visits-aa', 'ios-visitors-aa', 'ios-visits-ga', 'ios-visitors-ga']]    
-
     print('> get_data() -', 'data loaded')
     return df
 
 def get_data_events(df_aa):
-    print('')
+    print('')    
     df = pd.merge(df_aa, result_events['df_ga_events'], on='event2',suffixes=('-aa', '-ga'), how='outer')
-    
+      
     # Clean dataframe
     df = df.fillna(0)
     df = df.astype({'web-aa':'int64', 'web-ga':'int64', 'and-aa':'int64', 'and-ga':'int64', 'ios-aa':'int64', 'ios-ga':'int64'})   
     
+    df_platform = df[['event2', 'web-aa', 'web-ga']]
+    result_events['df_web_events'] = df_platform.loc[~((df_platform['web-aa']==0) & (df_platform['web-ga']==0))]
+    df_platform = df[['event2', 'and-aa', 'and-ga']]
+    result_events['df_and_events'] = df_platform.loc[~((df_platform['and-aa']==0) & (df_platform['and-ga']==0))]
+    df_platform = df[['event2', 'ios-aa', 'ios-ga']]
+    result_events['df_ios_events'] = df_platform.loc[~((df_platform['ios-aa']==0) & (df_platform['ios-ga']==0))]
     print('> get_data_events() -', 'data loaded')
-    result_events['df_events'] = df
-    result_events['df_web_events'] = df[['event2', 'web-aa', 'web-ga']]
-    result_events['df_and_events'] = df[['event2', 'and-aa', 'and-ga']]
-    result_events['df_ios_events'] = df[['event2', 'ios-aa', 'ios-ga']]   
+    return df
  
 # =============================================================================
 #   MAIN
@@ -178,7 +182,7 @@ result = {}
 result_events = {}
 variables = {}
 variables['rs'] = {}
-variables['token'] = 'eyJhbGciOiJSUzI1NiIsIng1dSI6Imltc19uYTEta2V5LWF0LTEuY2VyIiwia2lkIjoiaW1zX25hMS1rZXktYXQtMSIsIml0dCI6ImF0In0.eyJpZCI6IjE2NDYzMTYyOTc3MzRfMmYxZDE1YTYtYTcyYy00NWVmLWIyYTEtNDVhYzU5OTY3ZDhlX3VlMSIsInR5cGUiOiJhY2Nlc3NfdG9rZW4iLCJjbGllbnRfaWQiOiI1ZTlmZDU1ZmE5MmM0YTBhODJiM2YyYTc0YzA4OGU2MCIsInVzZXJfaWQiOiJBRDRBN0ExRDYwODhGOUY0MEE0OTVDNjhAdGVjaGFjY3QuYWRvYmUuY29tIiwiYXMiOiJpbXMtbmExIiwiYWFfaWQiOiJBRDRBN0ExRDYwODhGOUY0MEE0OTVDNjhAdGVjaGFjY3QuYWRvYmUuY29tIiwiY3RwIjowLCJmZyI6IldIV09ZQ0o2RkxFNUlQVUNFTVFGUkhRQVNBPT09PT09IiwibW9pIjoiOGY3NTEwODgiLCJleHBpcmVzX2luIjoiODY0MDAwMDAiLCJzY29wZSI6Im9wZW5pZCxBZG9iZUlELHJlYWRfb3JnYW5pemF0aW9ucyxhZGRpdGlvbmFsX2luZm8ucHJvamVjdGVkUHJvZHVjdENvbnRleHQiLCJjcmVhdGVkX2F0IjoiMTY0NjMxNjI5NzczNCJ9.anxkX0l8T60EBDEsyEzNUJh4_3v9m3jqPuhVboVaFXYO80q-LcLwhsVshJthl6E2Ymmzxw41AHVToWNj6-PrZjMqTCd4OIYnbjFa8JGdG7ndgjtpS066_DZIMAY6d533c8kxlvQN5g1XIxsH7Ptth1omO2z8EH2XxanURQycGICOrz-GrbfXzGwLlO5rxFkvJAuoFrtc6gTKhbYvhSi6KGnrbx9Abua4Md_CBox9TGYGwbCHEt3VksEDFzkYvvl30hPjvxNpr1tD7SyHK5M1-uf1UXK_Rdon4Bj9Op-J3KN7aDI5sLguMPo1zPb6VZwEzEWo-FtwIEEU5IWiTTLgEw'
+variables['token'] = 'eyJhbGciOiJSUzI1NiIsIng1dSI6Imltc19uYTEta2V5LWF0LTEuY2VyIiwia2lkIjoiaW1zX25hMS1rZXktYXQtMSIsIml0dCI6ImF0In0.eyJpZCI6IjE2NDY2NTUzMzY0NDhfODExNDBlZGItMWU3Yy00NTkyLThlOTctNmViOWE2M2M5YWFlX3VlMSIsInR5cGUiOiJhY2Nlc3NfdG9rZW4iLCJjbGllbnRfaWQiOiI1ZTlmZDU1ZmE5MmM0YTBhODJiM2YyYTc0YzA4OGU2MCIsInVzZXJfaWQiOiJBRDRBN0ExRDYwODhGOUY0MEE0OTVDNjhAdGVjaGFjY3QuYWRvYmUuY29tIiwiYXMiOiJpbXMtbmExIiwiYWFfaWQiOiJBRDRBN0ExRDYwODhGOUY0MEE0OTVDNjhAdGVjaGFjY3QuYWRvYmUuY29tIiwiY3RwIjowLCJmZyI6IldJQlA2Q0o2RkxFNUlQVUNFTVFGUkhRQVNBPT09PT09IiwibW9pIjoiMWE3NzUzNTkiLCJleHBpcmVzX2luIjoiODY0MDAwMDAiLCJjcmVhdGVkX2F0IjoiMTY0NjY1NTMzNjQ0OCIsInNjb3BlIjoib3BlbmlkLEFkb2JlSUQscmVhZF9vcmdhbml6YXRpb25zLGFkZGl0aW9uYWxfaW5mby5wcm9qZWN0ZWRQcm9kdWN0Q29udGV4dCJ9.VrTF-vRZ0bdoVjYf1iS__G6d0FzIMqujEMAmWGOIAck0pKfHQSpfLGzWftN0Cvp8ucAdKVcWV10fDFF6mTeAidLLerw01Zf6_LZ9P3uAZ1BJGB31A9a_t7gOALmKt_wOWnat7TWKfJikxQRqWZotWWZdBjx1AICLnKVt0m04nhBlhIgP2XcrXCPeEe9ipYjBq1UkSjJxdFksHkCOgZhu3zdLU0zRNfveiDIYccqERRPnwRDavHE46lKyCkgpyPPK31hA93-prGerWi7hYdafM450ezfsZG4TEnc1PJAcqhRX6M90LRV7eVuiQM_Ox2Lsz5vIZC6les9ALnkXyPaBzA'
 variables['directory'] = '/Users/luis.salamo/Documents/github enterprise/python-training/adobe/health-metrics-comparison'
 variables['rs']['rs_fotocasaes'] = 'vrs_schibs1_fcall'
 variables['rs']['rs_motosnet'] = 'vrs_schibs1_motorcochesnet'
@@ -195,34 +199,33 @@ result_events['df_aa_events'] = get_adobe_analytics_data_events()
 df_aa_events_fc = result_events['df_aa_events'].loc[result_events['df_aa_events'].rs == variables['rs']['rs_fotocasaes']]
 result_events['df_ga_events'] = get_google_analytics_data_events()
 result_events['df'] = get_data_events(df_aa_events_fc)
+# result_events['df_aa_events_fc'] = result_events['df_aa_events'].loc[result_events['df_aa_events'].rs == variables['rs']['rs_fotocasaes']]
 
-result_events['df_aa_events_fc'] = result_events['df_aa_events'].loc[result_events['df_aa_events'].rs == variables['rs']['rs_fotocasaes']]
+# summary = {}
+# summary['count_aa_events_web'] = len(result_events['df_web_events'].loc[result_events['df_web_events']['web-aa'] > 0])
+# summary['count_aa_events_and'] = len(result_events['df_and_events'].loc[result_events['df_and_events']['and-aa'] > 0])
+# summary['count_aa_events_ios'] = len(result_events['df_ios_events'].loc[result_events['df_ios_events']['ios-aa'] > 0])
 
-summary = {}
-summary['count_aa_events_web'] = len(result_events['df_web_events'].loc[result_events['df_web_events']['web-aa'] > 0])
-summary['count_aa_events_and'] = len(result_events['df_and_events'].loc[result_events['df_and_events']['and-aa'] > 0])
-summary['count_aa_events_ios'] = len(result_events['df_ios_events'].loc[result_events['df_ios_events']['ios-aa'] > 0])
-
-summary['count_ga_events_web'] = len(result_events['df_web_events'].loc[result_events['df_web_events']['web-ga'] > 0])
-summary['count_ga_events_and'] = len(result_events['df_and_events'].loc[result_events['df_and_events']['and-ga'] > 0])
-summary['count_ga_events_ios'] = len(result_events['df_ios_events'].loc[result_events['df_ios_events']['ios-ga'] > 0])
+# summary['count_ga_events_web'] = len(result_events['df_web_events'].loc[result_events['df_web_events']['web-ga'] > 0])
+# summary['count_ga_events_and'] = len(result_events['df_and_events'].loc[result_events['df_and_events']['and-ga'] > 0])
+# summary['count_ga_events_ios'] = len(result_events['df_ios_events'].loc[result_events['df_ios_events']['ios-ga'] > 0])
 
 # Events blocked by platform > pending to fix and split by plantform (not only web devices)
 # summary['count_aa_events_web'] = len(result_events['df_web_events'].loc[result_events['df_web_events']['web-aa'] > 0])
 # summary['count_aa_events_and'] = len(result_events['df_and_events'].loc[result_events['df_and_events']['and-aa'] > 0])
 # summary['count_aa_events_ios'] = len(result_events['df_ios_events'].loc[result_events['df_ios_events']['ios-aa'] > 0])
 
-df = result_events['df_web_events']
-summary['df_new_events_web'] = df.loc[(df['web-aa'] == 0) & (df['web-ga'] > 0)]
-summary['count_new_events_web'] = len(summary['df_new_events_web'])
-df = result_events['df_and_events']
-summary['df_new_events_and'] = df.loc[(df['and-aa'] == 0) & (df['and-ga'] > 0)]
-summary['count_new_events_and'] = len(summary['df_new_events_and'])
-df = result_events['df_ios_events']
-summary['df_new_events_ios'] = df.loc[(df['ios-aa'] == 0) & (df['ios-ga'] > 0)]
-summary['count_new_events_ios'] = len(summary['df_new_events_ios'])
+# df = result_events['df_web_events']
+# summary['df_new_events_web'] = df.loc[(df['web-aa'] == 0) & (df['web-ga'] > 0)]
+# summary['count_new_events_web'] = len(summary['df_new_events_web'])
+# df = result_events['df_and_events']
+# summary['df_new_events_and'] = df.loc[(df['and-aa'] == 0) & (df['and-ga'] > 0)]
+# summary['count_new_events_and'] = len(summary['df_new_events_and'])
+# df = result_events['df_ios_events']
+# summary['df_new_events_ios'] = df.loc[(df['ios-aa'] == 0) & (df['ios-ga'] > 0)]
+# summary['count_new_events_ios'] = len(summary['df_new_events_ios'])
 
-df_sumary = pd.DataFrame(summary.items())
+# df_sumary = pd.DataFrame(summary.items())
 
 # # =============================================================================
 # #   REQUEST GOOGLE ANALYTICS
